a12f30a9fb17879f1258223e99a7a5fdde722982
mdemon420 <mdeamon4@gmail.com>
2021-05-12 02:08:48 +0600

    arrow funtion

---
+++ arrow.js
@@ -0,0 +1,24 @@
+// function dubleIt(number){
+//  return number * 2;
+// }
+// const result = dubleIt(25);
+
+
+
+// const dubleIt = function(number){
+//     return number *2
+// }
+
+// arrow funtion
+const dubleIt = number => number*2;
+const result = dubleIt(25);
+console.log(result);
+
+const add = (x,y) => x + y;
+const result2 =  add(20,40);
+console.log(result2);
+
+const give10 = () => 5;
+const result3 =  give10();
+console.log(result3);
+
