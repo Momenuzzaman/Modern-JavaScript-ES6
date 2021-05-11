adbe9b553fefefffd07bc90a75cf84cee484b791
mdemon420 <mdeamon4@gmail.com>
2021-05-12 01:30:37 +0600

    template

---
+++ template.js
@@ -0,0 +1,6 @@
+const firstName = "Justin";
+const lastName = "Timber";
+const fullName =  firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName } ${lastName} is a good boy`;
+console.log(fullName);
+console.log(fullName2);
