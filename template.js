1438507967dc50458d8d1c6f75af2a8a0b84dca6
mdemon420 <mdeamon4@gmail.com>
2021-05-12 01:36:49 +0600

    multi Line

--- template.js
+++ template.js
@@ -4,3 +4,14 @@ const fullName =  firstName + " " + lastName + " is a good boy";
 const fullName2 = `${firstName } ${lastName} is a good boy`;
 console.log(fullName);
 console.log(fullName2);
+
+const multiLine = "I love You\n"
++ "I miss You\n"
++ "I need You";
+console.log(multiLine);
+
+const multiLine2 = `I love you
+i miss you
+i dont't need you`;
+
+console.log(multiLine2);
