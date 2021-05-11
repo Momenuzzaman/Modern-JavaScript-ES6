5d02cd06b8f73689fed537f1ccd893a1b6474185
mdemon420 <mdeamon4@gmail.com>
2021-05-12 00:36:32 +0600

    first commit

---
+++ letConst.js
@@ -0,0 +1,15 @@
+const hero = "Hero Alom";
+console.log(hero);
+
+const number = [11,21];
+number[1] = 22;
+console.log(number);
+number.push(33);
+console.log(number);
+
+let patientName = "Rahim";
+patientName = "korim";
+console.log(patientName);
+
+
+
