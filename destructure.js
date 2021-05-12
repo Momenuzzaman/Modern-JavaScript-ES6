5abae055a94f1cf0a7534e4c80f4a2a24ca3f57a
mdemon420 <mdeamon4@gmail.com>
2021-05-13 02:49:14 +0600

    destructure

---
+++ destructure.js
@@ -0,0 +1,6 @@
+const person = {name:"Jack Wiliam", age: 20, job: "facebook", wife: "Ema Watson", phone: 1122334455, home: "USA", friends: ["fahim", "karim", "jamil"]};
+
+const {name , age , job} = person;
+console.log(name,age,job);
+
+
