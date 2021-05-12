82914d2ec8fd74cd164bc417527434d007269383
mdemon420 <mdeamon4@gmail.com>
2021-05-13 00:30:58 +0600

    class add

---
+++ class.js
@@ -0,0 +1,11 @@
+class student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.Name = sName;
+        this.School = "ABCD School";
+    }
+}
+const student1 = new student( 1,"Al-Amin");
+const student2 = new student(2, "Ruhi");
+console.log(student1,student2);
+
